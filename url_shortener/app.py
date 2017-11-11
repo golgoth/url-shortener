dd4897337a0168255ecfc3827d3b80a6277b2983
golgoth <adrien.duffy@gmail.com>
2017-11-11 23:16:02 +0100

    Support for redirection

--- url_shortener/app.py
+++ url_shortener/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 import flask
 from .shortener import hash_url
 from .db import DB
 
 app = Flask(__name__)
+
 
 @app.before_request
 def init_db():
@@ -12,9 +13,19 @@ def init_db():
         app.logger.info('Creating database connection')
         flask.g.database.init_db()
 
+
 @app.route('/')
 def index():
     return render_template('index.html.j2', api_url=url_for('shorten_url'))
+
+
+@app.route('/<url_short>', methods=['GET', ])
+def redirect_short_url(url_short):
+    long_url = flask.g.database.get_long_url(url_short)
+    app.logger.info(f'Redirecting to {long_url}')
+    if long_url is None:
+        return redirect(url_for('index'))
+    return redirect(long_url)
 
 
 @app.route('/api/shorten', methods=['POST', ])
@@ -28,10 +39,12 @@ def shorten_url():
     try:
         # Check short string collision, TODO: collisions on md5
         while (long_url_existing != long_url_to_shorten) and (short_url_size < len(hashed_url)):
+            app.logger.info('getting old url new row')
             long_url_existing = flask.g.database.get_long_url(short_url)
             # It's the first time we input this short url
             if long_url_existing is None:
-                flask.g.database.insert_url(hashed_url, long_url_to_shorten)
+                flask.g.database.insert_url(short_url, long_url_to_shorten)
+                app.logger.info('Inserting new row')
                 break
             # There is already an existing short utl for different long url
             elif long_url_existing != long_url_to_shorten:
@@ -41,7 +54,7 @@ def shorten_url():
                 break
     except Exception as e:
         app.logger.error('Impossible to shorten the url because of a database problem', e)
-        #flask.flash('Server error, please retry later', category='warning')
+        # flask.flash('Server error, please retry later', category='warning')
         return render_template('index.html.j2')
 
     return f"Your short URL for {long_url_to_shorten} is {short_url}.\n"
--- url_shortener/db.py
+++ url_shortener/db.py
@@ -4,15 +4,15 @@ import boto3
 class DB:
     def __init__(self, endpoint_url, name_url_table):
         self.client = boto3.client('dynamodb',
-                                        endpoint_url=endpoint_url,
-                                        region_name='eu-west-1',
-                                        aws_access_key_id='anything',
-                                        aws_secret_access_key='anything')
-        self.ressource = boto3.resource('dynamodb',
-                                        endpoint_url=endpoint_url,
-                                        region_name='eu-west-1',
-                                        aws_access_key_id='anything',
-                                        aws_secret_access_key='anything')
+                                   endpoint_url=endpoint_url,
+                                   region_name='eu-west-1',
+                                   aws_access_key_id='anything',
+                                   aws_secret_access_key='anything')
+        self.resource = boto3.resource('dynamodb',
+                                       endpoint_url=endpoint_url,
+                                       region_name='eu-west-1',
+                                       aws_access_key_id='anything',
+                                       aws_secret_access_key='anything')
         self.name_url_table = name_url_table
 
     def init_db(self):
@@ -20,7 +20,7 @@ class DB:
 
     def _create_table_if_not_exist(self):
         if self.name_url_table not in self.client.list_tables()['TableNames']:
-            url_table = self.ressource.create_table(
+            url_table = self.resource.create_table(
                         TableName=self.name_url_table,
                         KeySchema=[
                             {
@@ -43,7 +43,7 @@ class DB:
             url_table.meta.client.get_waiter('table_exists').wait(TableName=self.name_url_table)
 
     def insert_url(self, short_url, long_url):
-        url_table = self.ressource.Table(self.name_url_table)
+        url_table = self.resource.Table(self.name_url_table)
         url_table.put_item(
                Item={
                     'short_url': short_url,
@@ -52,10 +52,11 @@ class DB:
             )
 
     def get_long_url(self, short_url: str):
-        url_table = self.ressource.Table(self.name_url_table)
+        url_table = self.resource.Table(self.name_url_table)
         row = url_table.get_item(
             Key={
                  'short_url': short_url
             }
         )
-        return row.get('long_url')
+        item = row.get('Item')
+        return None if item is None else item.get('long_url')
