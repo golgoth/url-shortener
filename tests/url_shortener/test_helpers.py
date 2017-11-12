b5a4f0f89337b173158f647f9011a19d958a8fda
golgoth <adrien.duffy@gmail.com>
2017-11-12 12:08:00 +0100

    test crash test

--- tests/url_shortener/test_helpers.py
+++ tests/url_shortener/test_helpers.py
@@ -5,4 +5,4 @@ def test_base_62_encode():
     assert helpers.base_encoding(61) == 'Z'
     assert helpers.base_encoding(0) == '0'
     assert helpers.base_encoding(62) == '10'
-    assert helpers.base_encoding(134) == '2a'
+    assert helpers.base_encoding(134) == '1a'
