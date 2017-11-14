7606a0bee1607ff15aa729a3e510df4076006238
golgoth <adrien.duffy@gmail.com>
2017-11-14 21:50:17 +0100

    adding copy to clipboard and prevent user input empty

--- url_shortener/static/js/index.js
+++ url_shortener/static/js/index.js
@@ -1,5 +1,16 @@
-function copyToClipboard() {
-  var copyText = document.getElementById("shortUrl");
-  copyText.select();
-  document.execCommand("Copy");
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
+
+function checkvalue() {
+    var mystring = $('#urlLong').val();
+    if(!mystring.match(/\S/)) {
+        return false;
+    } else {
+        return true;
+    }
 }
